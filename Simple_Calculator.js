f835d3ffa1e4fda7b7808bc3db8cf8f102ae0c6d
ManojKumar-343 <manojarumai31@gmail.com>
2026-04-01 20:53:57 +0530

    Simple_Calculator

---
+++ Simple_Calculator.js
@@ -0,0 +1,13 @@
+// Operators
+
+let a = 22;
+let b = 34;
+let c = a + b;
+console.log("The sum of a and b is " + c);
+
+let x = 10;
+let y = 2;
+console.log("The Div or Quotient of x and y is " + (x / y));
+
+let p = 11;
+console.log("The Modulo or Remainder of p and y is " + p % y);
